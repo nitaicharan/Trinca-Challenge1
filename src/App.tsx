9d0c5e125e3bc831e9a5206c01dff1acdbc8ec24
Nitai Charan <nitaicharan@gmail.com>
2021-07-03 00:00:00 -0300

    Fixed Login and Header responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Header } from './layouts/Header';
 import { Login } from './pages/Login';
 
 function App() {
   return (
     <Router>
+      <Route path="*" component={Header} />
       <Switch>
         <Route path="/" component={Login} />
       </Switch>
