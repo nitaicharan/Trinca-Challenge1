d5f223285ca7ffe660f7c5ec19593ee249972ffb
Nitai Charan <nitaicharan@gmail.com>
2021-08-12 00:00:00 -0300

    Fixed LocalStorage load AUTHORIZATION value

--- src/services/LocalStorageService.ts
+++ src/services/LocalStorageService.ts
@@ -3,5 +3,5 @@ import { Authentication } from "../interfaces/Authentication";
 const setItem = (params: { [key: string]: any }, key: string) => localStorage.setItem(key, JSON.stringify(params));
 export const saveAuth = (auth: Authentication) => setItem(auth, 'AUTHORIZATION_KEY');
 
-const getItem = (key: string) => localStorage.getItem(key) ?? '';
-export const getAuth = () => JSON.parse(getItem('AUTHORIZATION_KEY')) as Authentication;
+const getItem = (key: string) => localStorage.getItem(key);
+export const getAuth = () => JSON.parse(getItem('AUTHORIZATION_KEY') || '{}') as Authentication;
